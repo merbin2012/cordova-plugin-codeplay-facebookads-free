24c9e062753d866e385456aa44710dbc9a83f128
Merbin Joe <merbin2010@gmail.com>
2021-08-21 17:55:29 +0530

    Update codeplayfacebookads.js

--- www/codeplayfacebookads.js
+++ www/codeplayfacebookads.js
@@ -3,24 +3,18 @@ var exec = require('cordova/exec');
 exports.showBannerAds = function (arg0, success, error) {
     exec(success, error, 'codeplayfacebookads', 'showBannerAds',[arg0]);
 };
-
-
 exports.hideBannerAds = function (arg0,success, error) {
     exec(success, error, 'codeplayfacebookads', 'hideBannerAds',[arg0]);
 };
-
 exports.loadInterstitialAds = function (arg0, success, error) {
     exec(success, error, 'codeplayfacebookads', 'loadInterstitialAds', [arg0]);
 };
-
 exports.showInterstitialAds = function (arg0,success, error) {
     exec(success, error, 'codeplayfacebookads', 'showInterstitialAds',[arg0]);
 };
-
 exports.loadRewardVideoAd = function (arg0,success, error) {
     exec(success, error, 'codeplayfacebookads', 'loadRewardVideoAd',[arg0]);
 };
-
 exports.showRewardVideoAd = function (success, error) {
     exec(success, error, 'codeplayfacebookads', 'showRewardVideoAd',[""]);
 };
