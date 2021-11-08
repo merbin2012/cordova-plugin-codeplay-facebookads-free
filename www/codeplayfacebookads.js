789099c672c176b54484d7265e8333aea977d534
Merbin Joe <merbin2010@gmail.com>
2021-11-08 12:55:42 +0530

    Update codeplayfacebookads.js

--- www/codeplayfacebookads.js
+++ www/codeplayfacebookads.js
@@ -1,20 +1,48 @@
 var exec = require('cordova/exec');
 
-exports.showBannerAds = function (arg0, success, error) {
-    exec(success, error, 'codeplayfacebookads', 'showBannerAds',[arg0]);
+exports.loadAndShowBannerAds = function (arg0, success, error) {
+    exec(success, error, 'codeplayfacebookads', 'loadAndShowBannerAds',[arg0]);
 };
-exports.hideBannerAds = function (arg0,success, error) {
-    exec(success, error, 'codeplayfacebookads', 'hideBannerAds',[arg0]);
+
+
+exports.loadBannerAds = function (arg0, success, error) {
+    exec(success, error, 'codeplayfacebookads', 'loadBannerAds',[arg0]);
 };
+
+
+exports.showBannerAds = function (success, error) {
+    exec(success, error, 'codeplayfacebookads', 'showBannerAds');
+};
+
+
+
+
+
+exports.hideBannerAds = function (success, error) {
+    exec(success, error, 'codeplayfacebookads', 'hideBannerAds');
+};
+
+
+
+
+
+exports.distroyBannerAds = function (success, error) {
+    exec(success, error, 'codeplayfacebookads', 'distroyBannerAds');
+};
+
 exports.loadInterstitialAds = function (arg0, success, error) {
     exec(success, error, 'codeplayfacebookads', 'loadInterstitialAds', [arg0]);
 };
-exports.showInterstitialAds = function (arg0,success, error) {
-    exec(success, error, 'codeplayfacebookads', 'showInterstitialAds',[arg0]);
+
+exports.showInterstitialAds = function (success, error) {
+    exec(success, error, 'codeplayfacebookads', 'showInterstitialAds');
 };
+
 exports.loadRewardVideoAd = function (arg0,success, error) {
     exec(success, error, 'codeplayfacebookads', 'loadRewardVideoAd',[arg0]);
 };
+
 exports.showRewardVideoAd = function (success, error) {
-    exec(success, error, 'codeplayfacebookads', 'showRewardVideoAd',[""]);
+    exec(success, error, 'codeplayfacebookads', 'showRewardVideoAd');
 };
+
